90a1650389f802143dbd2c2dd9e02e03bc1abcfb
Dickson Freitas <dickfreitas12@gmaill.com>
2024-03-19 11:03:55 -0300

    adding the UPDATE method

--- jdbc-crud/src/application/Update.java
+++ jdbc-crud/src/application/Update.java
@@ -1,4 +1,37 @@
 package application;
 
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Update {
+    public static void main(String[] args) {
+        Connection connect = null;
+        PreparedStatement preparedStatement = null;
+
+        try{
+            connect = DB.getConnection();
+
+            preparedStatement = connect.prepareStatement(
+                    "UPDATE seller " + //Necessario colocar os espaços no final das ""
+                            "SET BaseSalary = BaseSalary + ? "+ //Necessario colocar os espaços no final das ""
+                            "WHERE " + //Necessario colocar os espaços no final das ""
+                            "(DepartmentId = ?)");
+
+            preparedStatement.setDouble(1 , 200.0);
+            preparedStatement.setInt(2 , 2);
+
+            int rowsAffect = preparedStatement.executeUpdate();
+            System.out.println("Done!! Rows Affect" + rowsAffect);
+        }catch (SQLException e ){
+            e.printStackTrace();
+        }finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeConnection();
+        }
+
+    }
 }
+
